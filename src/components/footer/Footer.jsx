bb7dc19634096e0c95dc1f39fa74d6f9dfe8beb4
Mojammel Molla <Mojammelmolla897@gmail.com>
2024-01-18 20:39:52 +0600

    Footer added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,14 +1,12 @@
 const Footer = () => {
   return (
-    <div>
-      <footer className="py-6 md:py-8">
-        <div className="container mx-auto">
-          <p className="text-center text-sm lg:text-base">
-            Copyright ©2023 | All rights reserved by Mojammel Molla
-          </p>
-        </div>
-      </footer>
-    </div>
+    <footer className="py-6 md:py-8">
+      <div className="container mx-auto">
+        <p className="text-center text-sm lg:text-base">
+          Copyright ©2023 | All rights reserved by Mojammel Molla
+        </p>
+      </div>
+    </footer>
   );
 };
 
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,3 +1,4 @@
+import Footer from '../components/footer/Footer';
 import Header from '../components/header/Header';
 import Navbar from '../components/navbar/Navbar';
 
@@ -6,6 +7,7 @@ const Home = () => {
     <div className="relative font-[Manrope] before:fixed before:left-0 before:top-0 before:-z-10 before:h-[435px] before:w-full before:rounded-bl-3xl before:bg-[#EAE6D7] max-md:px-4 lg:text-lg before:lg:rounded-bl-[79px]">
       <Navbar></Navbar>
       <Header></Header>
+      <Footer></Footer>
     </div>
   );
 };
