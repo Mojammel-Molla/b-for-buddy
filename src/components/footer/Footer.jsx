6ccacc19573dd86702617645c066a09efa3e7386
Mojammel Molla <Mojammelmolla897@gmail.com>
2024-01-18 20:34:29 +0600

    Navbar and header cretaed

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <div>
+      <footer className="py-6 md:py-8">
+        <div className="container mx-auto">
+          <p className="text-center text-sm lg:text-base">
+            Copyright ©2023 | All rights reserved by Mojammel Molla
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
