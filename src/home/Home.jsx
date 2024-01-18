578a7630ee5aca519e34d1d63acbe964ff2eeb98
Mojammel Molla <Mojammelmolla897@gmail.com>
2024-01-18 21:32:20 +0600

    Load books data from json

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,12 +1,22 @@
 import Footer from '../components/footer/Footer';
 import Header from '../components/header/Header';
+import Main from '../components/main/Main';
 import Navbar from '../components/navbar/Navbar';
+import { useEffect, useState } from 'react';
 
 const Home = () => {
+  const [allBooks, setAllBooks] = useState([]);
+  useEffect(() => {
+    fetch('/books.json')
+      .then(res => res.json())
+      .then(data => setAllBooks(data));
+  }, []);
+  console.log(allBooks);
   return (
     <div className="relative font-[Manrope] before:fixed before:left-0 before:top-0 before:-z-10 before:h-[435px] before:w-full before:rounded-bl-3xl before:bg-[#EAE6D7] max-md:px-4 lg:text-lg before:lg:rounded-bl-[79px]">
       <Navbar></Navbar>
       <Header></Header>
+      <Main allBooks={allBooks}></Main>
       <Footer></Footer>
     </div>
   );
